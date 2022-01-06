9e087f4a6c5ad2a27f6236035534fa92c44b8cea
Yi Hsuen <tayyihsuen@gmail.com>
2022-01-06 10:24:27 +0800

    Create task app

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+A web-application for managing tasks. Inspired by:
+* https://www.digitalocean.com/community/tutorials/how-to-set-up-a-ruby-on-rails-project-with-a-react-frontend
+* https://medium.com/quick-code/simple-rails-crud-app-with-react-frontend-using-react-rails-gem-b708b89a9419
---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Tasks</h1>
+        <p className="lead">
+          Time to do some tasks!
+        </p>
+        <hr className="my-4" />
+        <Link
+          to="/recipes"
+          className="btn btn-lg custom-button"
+          role="button"
+        >
+          View Tasks
+        </Link>
+      </div>
+    </div>
+  </div>
+);
